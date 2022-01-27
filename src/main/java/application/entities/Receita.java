76ab4895d994241cadf30e56a76c94ff4e9ac907
daniel <ddeniel1@yahoo.com.br>
2022-01-27 05:24:26 -0300

    Commit semana 1 pt1

---
+++ src/main/java/application/entities/Receita.java
@@ -0,0 +1,61 @@
+package application.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "receitas")
+public class Receita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "descricao", nullable = false, length = 256)
+    private String descricao;
+
+    @Column(name = "valor", nullable = false)
+    private Double valor;
+
+    @Convert(disableConversion = true)
+    @Column(name = "data", nullable = false)
+    private LocalDate data;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
